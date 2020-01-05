803e4bddd311cad2a3b6e5c1870beea9ae5c1cfb
dario mambro <dario.mambro@gmail.com>
2020-01-05 13:23:25 +0100

    2020

--- oversimple/FirOversampling.cpp
+++ oversimple/FirOversampling.cpp
@@ -1,5 +1,5 @@
 /*
-Copyright 2019 Dario Mambro
+Copyright 2019-2020 Dario Mambro
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
