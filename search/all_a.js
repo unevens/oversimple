d4928f977d5bf530cdfea29278b4e2bfe5feae78
dario mambro <dario.mambro@gmail.com>
2020-01-26 05:57:47 +0100

    doc update

---
+++ search/all_a.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['upsampler2x4avxdouble_46',['Upsampler2x4AvxDouble',['../classhiir_1_1_upsampler2x4_avx_double.html',1,'hiir']]],
+  ['upsampler2x8avx_47',['Upsampler2x8Avx',['../classhiir_1_1_upsampler2x8_avx.html',1,'hiir']]]
+];
---
+++ search/pages_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['oversimple_104',['oversimple',['../index.html',1,'']]]
+];
